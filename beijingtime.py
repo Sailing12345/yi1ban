a161f95348475f6a5bb469414441ae2461e930e5
Sailing12345 <69495257+Sailing12345@users.noreply.github.com>
2021-04-12 16:01:48 +0800

    Update beijingtime.py

--- beijingtime.py
+++ beijingtime.py
@@ -1,22 +1,41 @@
-import os
-import ntplib
-import datetime
-c = ntplib.NTPClient()
-hosts = ['edu.ntp.org.cn', 'tw.ntp.org.cn', 'us.ntp.org.cn', 'cn.pool.ntp.org', 'jp.ntp.org.cn']
-for host in hosts:
-    try:
-        response = c.request(host)
-        if response:
-            break
-    except Exception as e:
-        pass
-current_time = response.tx_time
-_date, _time = str(datetime.datetime.fromtimestamp(current_time))[:22].split(' ')
-print("系统当前时间", str(datetime.datetime.now())[:22])
-print("北京标准时间", _date, _time)
-a, b, c = _time.split(':')
-c = float(c) + 0.5
-_time = "%s:%s:%s" % (a, b, c)
-os.system('date %s && time %s' % (_date, _time))
-print("同步后时间:", _date, _time)
-
+import time
+import httplib
+import threading
+def getBeijinTime():
+   try:
+     conn = httplib.HTTPConnection("www.beijing-time.org")
+     conn.request("GET", "/time.asp")
+     response = conn.getresponse()
+     print (response.status, response.reason)
+     if response.status == 200:
+       result = response.read()
+       data = result.split("\r\n")
+       year = data[1][len("nyear")+1 : len(data[1])-1]
+       month = data[2][len("nmonth")+1 : len(data[2])-1]
+       day = data[3][len("nday")+1 : len(data[3])-1]
+       #wday = data[4][len("nwday")+1 : len(data[4])-1]
+       hrs = data[5][len("nhrs")+1 : len(data[5])-1]
+       minute = data[6][len("nmin")+1 : len(data[6])-1]
+       sec = data[7][len("nsec")+1 : len(data[7])-1]
+       beijinTimeStr = "%s/%s/%s %s:%s:%s" % (year, month, day, hrs, minute, sec)
+       beijinTime = time.strptime(beijinTimeStr, "%Y/%m/%d %X")
+       return beijinTime
+   except:
+     return None
+def syncLocalTime():
+   """
+   同步本地时间
+   """
+   beijinTime = getBeijinTime()
+   if beijinTime is None:
+     timer = threading.Timer(30.0, syncLocalTime)
+     timer.start()
+   else:
+     tm_year, tm_mon, tm_mday, tm_hour, tm_min, tm_sec = beijinTime[:6]
+     import os
+     os.system("date %d-%d-%d" % (tm_year, tm_mon, tm_mday))#设置日期
+     os.system("time %d:%d:%d.0" % (tm_hour, tm_min, tm_sec))#设置时间
+if __name__=='__main__':
+  while True:
+    syncLocalTime()
+    time.sleep(30)
